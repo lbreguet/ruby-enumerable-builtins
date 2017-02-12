04f67d83e46456b7aaa67a2cd248cee0989ec0f9
lbreguet <louis-w@live.com>
2017-02-11 19:13:28 -0500

    Ruby enumerable intro
    -review what enumerable's have

--- bin/hash_list.rb
+++ bin/hash_list.rb
@@ -1,3 +1,22 @@
 # frozen_string_literal: true
 
 # hash as enumerable code along
+
+alpha = ('a'..'z').map(&:to_sym).freeze
+hash = {}
+
+(1..26).each_with_index do |number, index|
+  hash[alpha[index]] = number
+end
+
+p "hash is #{hash}"
+
+less_than_twenty_seven = hash.all? do |_key, value|
+  value < 27
+end
+
+p "less than 27 letters in the alphabet? #{less_than_twenty_seven}"
+
+# hash.values
+# hash.keys
+# hash.length
--- bin/range_list.rb
+++ bin/range_list.rb
@@ -1,3 +1,23 @@
 # frozen_string_literal: true
 
 # range as enumerable code along
+
+ints = 0..10
+
+puts "ints range is #{ints}"
+puts "ints max is #{ints.max}"
+puts "ints min is #{ints.min}"
+puts "ints max two elements are #{ints.max(2)}"
+
+alpha = 'a'..'z'
+
+puts "alpha range is #{alpha}"
+puts "alpha max is #{alpha.max}"
+puts "alpha min is #{alpha.min}"
+puts "alpha max two elements are #{alpha.max(2)}"
+
+every_third_alpha = alpha.group_by do |a|
+  (a.ord % 3).zero?
+end
+
+p "every third alpha is #{every_third_alpha[true]}"
